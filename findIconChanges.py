69fb2609385bde8f2e630f981ef846695b56f997
FancyEveryDay <157014935+FancyEveryDay@users.noreply.github.com>
2026-07-18 17:59:23 -0500

    rewrote get-patch number logic

--- findIconChanges.py
+++ findIconChanges.py
@@ -7,84 +7,93 @@ SINS_DIRECTORY = "f:\\SteamLibrary\\steamapps\\common\\Sins2"
 TEXTURES_LOCATION = SINS_DIRECTORY + "\\textures"
 SINS_FILES_LOCATION = "f:\\SinsFiles\\textures"
 
-newPatchNumber = input("New Patch Number [format 1.28.16]:  ")
-while True:
-    userInput = input(f"Is '{newPatchNumber}' the correct number for the current patch? [yes/no]")
+def get_patch_number():
+    newPatchNumber = input("New Patch Number [format 1.28.16]:  ")
+    while True:
+        userInput = input(f"Is '{newPatchNumber}' the correct number for the current patch? [yes/no]")
 
-    if userInput.strip().lower() in ['yes', 'y','yea', 'yeah']:
-        print("Acknowledged")
-        break
-    elif userInput.strip().lower() in ['no', 'n', 'nahp', 'nope', 'nay']:
-        newPatchNumber = input("Current Patch Number [format 1.28.16]:  ")
-    else:
-        print("Input not recognized, try again.")
-
-# Try to get pastpatch number from .env, otherwise from user.
-# try: 
-#     pastPatchNumber = env["pastPatch"]
-# except KeyError:
-print("No previous patch number found, please input one.")
-pastPatchNumber = input("Previous Patch Number [format 1.28.16]:  ")
-
-while True:
-    userInput = input(f"Is '{pastPatchNumber}' the correct number for the previous patch? [yes/no]")
-
-    if userInput.strip().lower() in ['yes', 'y','yea', 'yeah']:
-        print("Acknowledged")
-        break
-    elif userInput.strip().lower() in ['no', 'n', 'nahp', 'nope', 'nay']:
-        pastPatchNumber = input("Previous Patch Number [format 1.28.16]:  ")
-    else:
-        print("Input not recognized, try again.")
-
-# Copy over new set of files
-print("Copying new texture files")
-
-new_data_folder = f"{SINS_FILES_LOCATION}\\Patch {newPatchNumber[2:]} Textures"
-past_data_folder = f"{SINS_FILES_LOCATION}\\Patch {pastPatchNumber[2:]} Textures"
-
-try:
-    shutil.copytree(TEXTURES_LOCATION,  new_data_folder)
-except FileExistsError:
-    shutil.rmtree(new_data_folder)
-    shutil.copytree(TEXTURES_LOCATION, new_data_folder)
-
-# Change number formatting (for reasons)
-newPatchNumber = newPatchNumber.replace(".","-")
-pastPatchNumber = pastPatchNumber.replace(".","-")
-
-
-## Get files
-oldImages = glob.glob(f"{past_data_folder}\\**.png") #tooltip_picture200.png")
-newImages = glob.glob("f:\\SteamLibrary\\steamapps\\common\\Sins2\\textures\\**.png") #tooltip_picture200.png")
-
-shutil.rmtree(f"{SINS_FILES_LOCATION}\\New Textures")
-os.mkdir(f"{SINS_FILES_LOCATION}\\New Textures")
-
-oldImagesDict = {}
-
-for i in oldImages:
-    oldImagesDict[i.split("\\")[-1] ] = i
-
-newImagesDict = {}
-
-for i in newImages:
-    newImagesDict[i.split("\\")[-1] ] = i
-
-with open(f"Patch Archive\\{newPatchNumber}_new_textures.txt", "w") as file:
-    file.write(f"{"_"*40}\n{"New Textures for Patch " + newPatchNumber :^40}\n{"_"*40}\n\n")
-
-with open(f"Patch Archive\\{newPatchNumber}_new_textures.txt", "a") as file:
-    for i, newImage in newImagesDict.items():
-        oldImage = oldImagesDict.get(i, False)
-        if oldImage:
-            if filecmp.cmp(newImage, oldImage, shallow=False):
-                continue
-            else:
-                print(i)
-                file.write(i + "\n")
+        if userInput.strip().lower() in ['yes', 'y','yea', 'yeah']:
+            print("Acknowledged")
+            break
+        elif userInput.strip().lower() in ['no', 'n', 'nahp', 'nope', 'nay']:
+            newPatchNumber = input("Current Patch Number [format 1.28.16]:  ")
         else:
-            print("[NEW] " + i)
-            file.write("[NEW] " + i + "\n")
+            print("Input not recognized, try again.")
 
-        shutil.copy2(newImage, f"{SINS_FILES_LOCATION}\\New Textures")
+    # Try to get pastpatch number from .env, otherwise from user.
+    # try: 
+    #     pastPatchNumber = env["pastPatch"]
+    # except KeyError:
+    print("No previous patch number found, please input one.")
+    pastPatchNumber = input("Previous Patch Number [format 1.28.16]:  ")
+
+    while True:
+        userInput = input(f"Is '{pastPatchNumber}' the correct number for the previous patch? [yes/no]")
+
+        if userInput.strip().lower() in ['yes', 'y','yea', 'yeah']:
+            print("Acknowledged")
+            break
+        elif userInput.strip().lower() in ['no', 'n', 'nahp', 'nope', 'nay']:
+            pastPatchNumber = input("Previous Patch Number [format 1.28.16]:  ")
+        else:
+            print("Input not recognized, try again.")
+
+    return newPatchNumber, pastPatchNumber
+
+def find_icon_changes(newPatchNumber, pastPatchNumber):
+    # Copy over new set of files
+    print("Copying new texture files")
+
+    new_data_folder = f"{SINS_FILES_LOCATION}\\Patch {newPatchNumber[2:]} Textures"
+    past_data_folder = f"{SINS_FILES_LOCATION}\\Patch {pastPatchNumber[2:]} Textures"
+
+    try:
+        shutil.copytree(TEXTURES_LOCATION,  new_data_folder)
+    except FileExistsError:
+        shutil.rmtree(new_data_folder)
+        shutil.copytree(TEXTURES_LOCATION, new_data_folder)
+
+    # Change number formatting (for reasons)
+    newPatchNumber = newPatchNumber.replace(".","-")
+    pastPatchNumber = pastPatchNumber.replace(".","-")
+
+
+    ## Get files
+    oldImages = glob.glob(f"{past_data_folder}\\**.png") #tooltip_picture200.png")
+    newImages = glob.glob("f:\\SteamLibrary\\steamapps\\common\\Sins2\\textures\\**.png") #tooltip_picture200.png")
+
+    shutil.rmtree(f"{SINS_FILES_LOCATION}\\New Textures")
+    os.mkdir(f"{SINS_FILES_LOCATION}\\New Textures")
+
+    oldImagesDict = {}
+
+    for i in oldImages:
+        oldImagesDict[i.split("\\")[-1] ] = i
+
+    newImagesDict = {}
+
+    for i in newImages:
+        newImagesDict[i.split("\\")[-1] ] = i
+
+    with open(f"Patch Archive\\{newPatchNumber}_new_textures.txt", "w") as file:
+        file.write(f"{"_"*40}\n{"New Textures for Patch " + newPatchNumber :^40}\n{"_"*40}\n\n")
+
+    with open(f"Patch Archive\\{newPatchNumber}_new_textures.txt", "a") as file:
+        for i, newImage in newImagesDict.items():
+            oldImage = oldImagesDict.get(i, False)
+            if oldImage:
+                if filecmp.cmp(newImage, oldImage, shallow=False):
+                    continue
+                else:
+                    print(i)
+                    file.write(i + "\n")
+            else:
+                print("[NEW] " + i)
+                file.write("[NEW] " + i + "\n")
+
+            shutil.copy2(newImage, f"{SINS_FILES_LOCATION}\\New Textures")
+
+
+if __name__ == "__main__":
+    newPatchNumber, pastPatchNumber = get_patch_number()
+    find_icon_changes(newPatchNumber, pastPatchNumber)
